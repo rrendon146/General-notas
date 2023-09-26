7a2fec926a88fe8e660fea83eea951b83f0896ed
rrendon146 <84236859+rrendon146@users.noreply.github.com>
2023-09-26 18:40:35 -0500

    jqueryajax.js

--- jqueryajax.js
+++ jqueryajax.js
@@ -14,7 +14,13 @@ JAVASCRIPT
                             });
 //cerrar parentesis y llaves en esta seccion, previamente crear un boton en php
 
+	
+            <div id="imagen" style="background:green; min-height:100px;"> </div> 
+		//en caso de añadir una imagen dentro de la web y aparezca para los formatos JSON con imagenes, se crea un div dentro de home.php
+            
 
+            
+	
 function getValores2() //funcion creada 
 {
 	//
@@ -33,21 +39,23 @@ function getValores2() //funcion creada
             {
                 case 200:
                     //
-                console.log(json); //arroje formato json en consola en caso de 200http si todo esta ok
-                console.log(json.results.length); //para leer subcontenido o nodos dentro del formato json (results es el nombre del nodo de la url)(length subcarpetas en
-                                                    general//
+                console.log(json); //arroje formato json en consola en caso de 200http si todo esta ok, por lo general se ejecuta cuando no hay nodos
+             // console.log(json.results.length); /*para leer subcontenido o nodos dentro del formato json(results es el nombre del nodo de la url)(length subcarpetas)*/
+                                                	
 
-                let imagen = $('#imagen');
+                //let imagen = $('#imagen'); // variable creada sacado del id div lineas anteriores
 
-                for (let index = 0; index < json.results.length; index++) {
-                    const element = json.results[index];
-                    let origin = element.origin;
-                    let html = '';
-                    html = `<img src="${element.image}" id="${element.id}" tittle="${element.name}"/>`;
-                    imagen.append(html);
-                    //console.log(element.url);
-                    console.log(element.id, element.gender);
-                    console.log(element.origin.name);
+                //for (let index = 0; index < json.results.length; index++) { //para ejecutar todo el formato json con nodos (ejemp .results.length)
+		for (let index = 0; index < json; index++) { //para ejecutar todo el formato json sin nodos
+                  //const element = json.results[index]; //el simbolo . se pone para leer los nodos dentro del formato json (ejem .results) y el index para leer todo
+		    const element = json[index]; // por lo general se ejecuta para leer todo el formato json sin nodos
+                 //  let origin = element.origin; //let variable que puede ser cambiada //agregado para el json de imagenes
+                   //let html = ''; //agregado para el json de imagenes
+                   //html = `<img src="${element.image}" id="${element.id}" tittle="${element.name}"/>`; //las comillas ``es para llamar javascript dentro de un element html
+                  //imagen.append(html);
+                  
+                    console.log(element.id, element.gender); // para imprimir una columna o varias columnas con la coma ","
+                //  console.log(element.origin.name); //para imprimir las subcarpetas o nodos dentro el punto "."
                 }
                 break;
                 case 202:
