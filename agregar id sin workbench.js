76a6c765f528f7681373a50e581ee0a5e688252c
Ricardo Rendon <84236859+rrendon146@users.noreply.github.com>
2024-05-24 18:40:39 -0500

    Create agregar id sin workbench.js

---
+++ agregar id sin workbench.js
@@ -0,0 +1,7 @@
+// no se llama al name para que al momento de guardar no vaya al POST
+                            <div class="form-group">
+                                <label for="Correo" >Correo</label> 
+                                <input type="text" id="Correo" class="form-control" maxlength="150" readonly/>
+                                </select>
+                            </div>
+
