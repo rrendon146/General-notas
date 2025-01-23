f4296c42cc27732e411c92eb3fb6edbfbd3f65f8
Ricardo Rendon <84236859+rrendon146@users.noreply.github.com>
2025-01-23 17:21:31 -0500

    Create nestjs impresion hojablanco.ts

---
+++ nestjs impresion hojablanco.ts
@@ -0,0 +1,6 @@
+/* impedir que hojas blancas se impriman */
+/* agregar el codigo al final del codigo de impresion */
+
+
+<!-- ESTO CREA UNA NUEVA PAGINA -->
+                <div style="page-break-after:always;" ></div>
